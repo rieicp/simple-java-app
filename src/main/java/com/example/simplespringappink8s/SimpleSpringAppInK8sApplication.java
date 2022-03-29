602006434b1d09a66d3f67c699ac589574067b29
Ning Wei <ning.wei@int-trade.de>
2022-03-29 18:57:27 +0200

    Initial commit

---
+++ src/main/java/com/example/simplespringappink8s/SimpleSpringAppInK8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringappink8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringAppInK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringAppInK8sApplication.class, args);
+    }
+
+}
